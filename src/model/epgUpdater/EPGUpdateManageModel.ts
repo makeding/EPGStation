f97a93c9c895a6bc25ef34cf824b847862873d5d
huggy <i@huggy.moe>
2026-01-18 21:57:17 +0900

    ?

--- src/model/epgUpdater/EPGUpdateManageModel.ts
+++ src/model/epgUpdater/EPGUpdateManageModel.ts
@@ -211,8 +211,8 @@ class EPGUpdateManageModel extends EventEmitter implements IEPGUpdateManageModel
             }
             this.channelIndex[service.networkId][service.serviceId] = {
                 id: service.id,
-                type: service.channel[0].type,
-                channel: service.channel[0].channel,
+                type: service.channel.type,
+                channel: service.channel.channel,
             };
         }
     }
